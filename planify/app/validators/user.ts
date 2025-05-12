763637577f50c0d9dd59637e8a3611173a6c0ebf
Hirwa Rene <hirwarene6@gmail.com>
2025-05-12 15:47:31 +0200

    added name properties for data validation

--- planify/app/validators/user.ts
+++ planify/app/validators/user.ts
@@ -1,1 +1,13 @@
-import vine from '@vinejs/vine'y
+import vine from '@vinejs/vine'
+
+/**
+ * Validates the user's creation action
+ */
+export const createUserValidator = vine.compile(
+  vine.object({
+    fullname: vine.string().trim().minLength(4).maxLength(50),
+    email: vine.string().trim().email().maxLength(50),
+    password: vine.string().trim().minLength(8).maxLength(20),
+  })
+)
+
