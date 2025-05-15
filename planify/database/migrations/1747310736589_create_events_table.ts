0aee28250e8e69197c03b8b8a64478c3fb7dda3c
Hirwa Rene <hirwarene6@gmail.com>
2025-05-15 14:37:19 +0200

    made and ran event migration

--- planify/database/migrations/1747310736589_create_events_table.ts
+++ planify/database/migrations/1747310736589_create_events_table.ts
@@ -6,6 +6,16 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').notNullable
+      table.string('category').notNullable
+      table.string('image').notNullable
+      table.string('title').notNullable
+      table.string('begin_time').notNullable
+      table.string('end_time').notNullable
+      table.date('begin_date').notNullable
+      table.date('end_date').notNullable
+      table.string('location').notNullable
+      table.string('description').notNullable
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
