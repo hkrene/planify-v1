daea708d094dd45193b2f4fd967f17324dd473a6
Hirwa Rene <hirwarene6@gmail.com>
2025-05-17 17:21:36 +0200

    passed login validation parameter in login validator

--- planify/app/validators/login.ts
+++ planify/app/validators/login.ts
@@ -1,1 +1,13 @@
+// import vine from '@vinejs/vine'
 import vine from '@vinejs/vine'
+
+/**
+ * Validates the user's creation action
+ */
+export const createUserValidator = vine.compile(
+  vine.object({
+    email: vine.string().trim().email().maxLength(50),
+    password: vine.string().trim().minLength(8).maxLength(20),
+  })
+)
+
