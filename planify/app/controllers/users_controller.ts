52ea1aaaac06696735309905f2df2c331975a22d
Hirwa Rene <hirwarene6@gmail.com>
2025-05-17 17:40:59 +0200

    added response.redirect to eventHome route after login

--- planify/app/controllers/users_controller.ts
+++ planify/app/controllers/users_controller.ts
@@ -29,25 +29,13 @@ export default class UsersController {
 
 
   public async login({view, request, response, auth}: HttpContext){
-    /**
-     * Step 1: Get credentials from the request body
-     */
     const { email, password } = request.only(['email', 'password'])
 
-    /**
-     * Step 2: Verify credentials
-     */
     const user = await User.verifyCredentials(email, password)
 
-    /**
-     * Step 3: Login user
-     */
     await auth.use('web').login(user)
 
-    /**
-     * Step 4: Send them to a protected route
-     */
-    response.redirect('/dashboard')
+    response.redirect('/eventHome')
 
   }
 }
